5f74df589c03b4420356e24850c42373276ac1c3
sameerhegde <109801628+sameerhegde@users.noreply.github.com>
2025-04-23 11:32:52 +0530

    Update apbMUX.v

--- apbMUX.v
+++ apbMUX.v
@@ -1,23 +1,23 @@
 module apbMUX
 	#(parameter DATAWIDTH = 32)(
-	input PSEL1in,PSEL2in;
-	input [DATAWIDTH-1:0]PRDATAin1,PRDATAin2;
-	output PSEL1out,PSEL1out,
-	output [DATAWIDTH-1:0]PRDATAout
+	input PSEL1in,PSEL2in,
+	input [DATAWIDTH-1:0]PRDATAin1,PRDATAin2,
+	output reg PSEL1out,PSEL2out,
+	output reg [DATAWIDTH-1:0]PRDATAout
 	);
 	
 	always@(*)begin
 		case({PSEL1in,PSEL2in})
 			2'b10:begin
-				{PSEL1out,PSEL1out} = 2'b10;
+				{PSEL1out,PSEL2out} = 2'b10;
 				PRDATAout = PRDATAin1;
 				end
 			2'b01:begin
-				{PSEL1out,PSEL1out} = 2'b01;
+				{PSEL1out,PSEL2out} = 2'b01;
 				PRDATAout = PRDATAin2;
 				end
 			default:begin
-				{PSEL1out,PSEL1out} = 2'b00;
+				{PSEL1out,PSEL2out} = 2'b00;
 				PRDATAout = 'b0;
 				end
 		endcase
